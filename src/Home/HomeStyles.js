cf9630911c526ecb2607d84bc5174ab3123fd57f
Mish <mishuct26@gmail.com>
2021-04-10 11:24:59 +0200

    Hero home styling

--- src/Home/HomeStyles.js
+++ src/Home/HomeStyles.js
@@ -5,5 +5,23 @@ export const HomeStyles = styled.div`
         text-align: center;
         max-width: 540px;
         margin: 0 auto 64px;
+
+        h2 {
+            font-size: 72px;
+            color: #18214D;
+            font-weight: 600;
+            letter-spacing: -3px;
+            margin-bottom: 16px;
+        }
+
+        p {
+            font-size: 20px;
+            line-height: 28px;
+            color: #3E4462;
+            letter-spacing: -0.4px;
+            max-width: 445px;
+            margin: 0 auto; 
+        }
     }
+
 `

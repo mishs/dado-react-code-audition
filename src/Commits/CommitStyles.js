ff1af6203c93c4f0dbafdd39f8b01d76ef343784
Mish <mishuct26@gmail.com>
2021-04-10 12:36:35 +0200

    Commit styles page styling

--- src/Commits/CommitStyles.js
+++ src/Commits/CommitStyles.js
@@ -1,7 +1,65 @@
 import styled from 'styled-components';
 
 export const CommitStyles = styled.div`
+    nav {
+        padding: 36px 0;
+
+        > div {
+            flex: 1;
+        }
+
+        form {
+            margin-right: 0;
+        }
+    }
+
     h3 {
         text-align: center;
+        font-size: 36px;
+        color: #29335C;
+        font-weight: 600;
+        letter-spacing: -1.5px;
+    }
+
+    ul {
+        padding: 0;
+        list-style: none;
+        color: #18214D;
+
+        li {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            span {
+                margin: 0 auto;
+                font-size: 20px;
+                letter-spacing: -0.4px;
+            }
+
+            > div {
+                display: flex;
+                align-items: center;
+            }
+
+            figure {
+                img {
+                    border-radius: 50%;
+                    height: 60px;
+                    width: 60px;
+                }
+
+                figcaption {
+                    font-size: 22px;
+                    font-weight: 600;
+                    letter-spacing: -0.55px;
+                }
+            }
+
+            p {
+                font-size: 20px;
+                letter-spacing: -0.4px;
+            }
+        }
     }
 `

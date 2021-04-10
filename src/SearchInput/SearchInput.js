fe2173e90b272d322b9fb2b5d2f7c1d9e57d2ee3
Mish <mishuct26@gmail.com>
2021-04-10 12:02:54 +0200

    Adds icons

--- src/SearchInput/SearchInput.js
+++ src/SearchInput/SearchInput.js
@@ -1,9 +1,9 @@
-import React, {useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { FaSearch } from 'react-icons/fa'
 import { SearchInputStyles } from './SearchInputStyles'
 import { useHistory } from 'react-router-dom';
 
-
-function SearchInput(props) {
+function SearchInput(props) {    
     const [query, setQuery]     = useState('');
     const { push }              = useHistory();
     
@@ -15,20 +15,22 @@ function SearchInput(props) {
     return (
         <SearchInputStyles>
             <form onSubmit={handleSubmit}>
-              <div>
-                <input type="text"
-                placeholder="Eg. facebook/react"
-                // value={}
-                onChange={(e) => setQuery(e.target.value)}
+                <div className="search-wrapper">
+                    <FaSearch />
+                    <input 
+                        type="text"
+                        placeholder="Eg. facebook/react"
+                        // value={}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                </div>
+
+                <input 
+                    type="submit" 
+                    value="See commits 🚀"              
                 />
-              </div>
-              <input
-                type="submit"
-                value="See commits"  
-                />
-            </form>
+            </form> 
         </SearchInputStyles>
-        
     )
 }
 

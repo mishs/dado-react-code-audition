354aa6ca7fb79fc6a53e6482d5a4c24ef66f036e
Mish <mishuct26@gmail.com>
2021-04-10 09:15:43 +0200

    Adds node-sass, react-router-dom and styled components

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { HeaderStyles } from './HeaderStyles'
+
+function Header() {
+    return (
+        <HeaderStyles>     
+            <nav>
+                <h1>
+                    <Link to="/">Commit Viewer</Link>     
+                </h1>            
+                <ul>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </HeaderStyles>
+    )
+}
+
+export default Header

a1b387ea74ac7a5e3f69825560331147a1c563d5
Mish <mishuct26@gmail.com>
2021-04-10 12:10:44 +0200

    Flesh-out repo tab-buttons

--- src/SuggestedRepos/SuggestedRepos.js
+++ src/SuggestedRepos/SuggestedRepos.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import { SuggestedReposStyles } from './SuggestedReposStyles'
+import { useHistory } from 'react-router-dom';
 
 function SuggestedRepos() {
+    const { push }              = useHistory();
+
+    const repos = [
+        {
+            title: 'django/django'
+        },
+        {
+            title: 'microsoft/vscode'
+        },
+        {
+            title: 'jezen/is-thirteen'
+        },
+        {
+            title: 'benawad/dogehouse'
+        }
+    ]
+
+    function handleClick(title) {
+        push('/commits');
+    }
+
     return (
         <SuggestedReposStyles>
-                        <span>Or pick one of these suggested repos</span>
+            <span>Or pick one of these suggested repos</span>
+
+            <ul>
+                { 
+                    repos.map((repo) => <li 
+                        onClick={() => handleClick(repo.title)}>
+                            {repo.title}
+                        </li>
+                    ) 
+                }
+            </ul>
         </SuggestedReposStyles>
-        
     )
 }
 

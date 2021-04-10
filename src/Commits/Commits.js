fb449f70124d5a255cb0358b6c42da74376ddcdf
Mish <mishuct26@gmail.com>
2021-04-10 16:36:10 +0200

    Passing fetched data to UI

--- src/Commits/Commits.js
+++ src/Commits/Commits.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import SearchInput from '../SearchInput/SearchInput'
@@ -7,58 +7,61 @@ import { CommitStyles } from './CommitStyles'
 import { CommitsContext } from '../context/CommitsContext'
 
 function Commits() {
-    const { searchQuery }           = useContext(CommitsContext);
-    const [commits, setCommits]     = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const { searchQuery } = useContext(CommitsContext)
+  const [commits, setCommits] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
-    useEffect(() => {   
-        if (searchQuery !== '') {
-            setIsLoading(true);
+  useEffect(() => {
+    if (searchQuery !== '') {
+      setIsLoading(true)
 
-            axios.get(`https://api.github.com/repos/${searchQuery}/commits?per_page=10`)
-            .then(response => {
-                setCommits(response.data);
-                console.log(response.data);
-                setIsLoading(false);
-            });
-        }
+      axios
+        .get(`https://api.github.com/repos/${searchQuery}/commits?per_page=10`)
+        .then((response) => {
+          setCommits(response.data)
+          console.log(response.data)
+          setIsLoading(false)
+        })
+    }
+  }, [searchQuery])
 
-        
-    },[searchQuery]);
+  console.log(commits)
 
-    console.log(commits)
-    
   return (
     <CommitStyles>
-            <HeaderStyles style={{ background: '#EFF2F6', marginBottom: '32px' }}>     
-                <nav className="container">
-                    <h1>
-                        <Link to="/">Commit Viewer</Link>     
+      <HeaderStyles style={{ background: '#EFF2F6', marginBottom: '32px' }}>
+        <nav className="container">
+          <h1>
+            <Link to="/">Commit Viewer</Link>
           </h1>
-          <SearchInput />
+          <SearchInput value={searchQuery} />
         </nav>
       </HeaderStyles>
 
       <div className="container">
-        <h3>microsoft/vscode</h3>
+        <h3>{searchQuery}</h3>
 
         <ul className="results">
-          <li>
-            <span className="loading">Loading...</span>
-          </li>
+          {isLoading === true ? (
+            <li>
+              <span className="loading">Loading...</span>
+            </li>
+          ) : (
+            commits.map((commit) => (
+              <li key={commit.sha}>
+                <div>
+                  <figure>
+                    <img src={commit.author.avatar_url} alt="avatar" />
+                    <figcaption>{commit.author.login}</figcaption>
+                  </figure>
 
-          <li>
-            <div>
-              <figure>
-                <img src alt="avatar" />
-                <figcaption>gaearon</figcaption>
-              </figure>
+                  <p>{commit.commit.message}</p>
+                </div>
 
-              <p>Log all error to console.error by default (#21130)</p>
-            </div>
-
-            <p>23:30 28/04/2021</p>
-          </li>
+                <p>{commit.commit.author.date}</p>
+              </li>
+            ))
+          )}
         </ul>
       </div>
     </CommitStyles>

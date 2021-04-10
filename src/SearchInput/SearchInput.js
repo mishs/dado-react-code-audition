04d50273ea60dfa72025f40822444deefc0231d4
Mish <mishuct26@gmail.com>
2021-04-10 12:39:42 +0200

    Search flesh-out, polish and code-formatting

--- src/SearchInput/SearchInput.js
+++ src/SearchInput/SearchInput.js
@@ -1,37 +1,34 @@
 import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { SearchInputStyles } from './SearchInputStyles'
-import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom'
 
-function SearchInput(props) {    
-    const [query, setQuery]     = useState('');
-    const { push }              = useHistory();
-    
-    function handleSubmit(e) {        
-        e.preventDefault();
-        push('/commits');
-    }
+function SearchInput(props) {
+  const [query, setQuery] = useState('')
+  const { push } = useHistory()
 
-    return (
-        <SearchInputStyles>
-            <form onSubmit={handleSubmit}>
-                <div className="search-wrapper">
-                    <FaSearch />
-                    <input 
-                        type="text"
-                        placeholder="Eg. facebook/react"
-                        // value={}
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                </div>
+  function handleSubmit(e) {
+    e.preventDefault()
+    push('/commits')
+  }
 
-                <input 
-                    type="submit" 
-                    value="See commits 🚀"              
-                />
-            </form> 
-        </SearchInputStyles>
-    )
+  return (
+    <SearchInputStyles>
+      <form onSubmit={handleSubmit}>
+        <div className="search-wrapper">
+          <FaSearch />
+          <input
+            type="text"
+            placeholder="Eg. facebook/react"
+            // value={}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+
+        <input type="submit" value="See commits 🚀" />
+      </form>
+    </SearchInputStyles>
+  )
 }
 
 export default SearchInput

2c070c8b8a4d320534c74d9200472087c5c40cbd
Mish <mishuct26@gmail.com>
2021-04-10 12:12:44 +0200

    Suggested repos tabs styling

--- src/SuggestedRepos/SuggestedReposStyles.js
+++ src/SuggestedRepos/SuggestedReposStyles.js
@@ -6,4 +6,29 @@ export const SuggestedReposStyles = styled.div`
     justify-content: center;
     flex-direction: column;
 
+    ul {
+        list-style: none;
+        padding: 0;
+        display: flex;
+        justify-content: center;
+        margin-top: 24px;
+
+        li {
+            margin-right: 16px;
+            background: #29335C;
+            color: #fff;
+            padding: 8px 16px;
+            border-radius: 8px;
+            font-weight: 600;
+            letter-spacing: -0.4px;
+            cursor: pointer;
+        }
+    }
+
+    span {
+        text-align: center;
+        font-size: 14px;
+        color: #3E4462;
+        letter-spacing: -0.4px;
+    }
 `

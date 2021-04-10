20ade6d1082578e1c55ab3dea9a472b2812ed7e5
Mish <mishuct26@gmail.com>
2021-04-10 09:35:28 +0200

    Routing and Linking whole app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
 
-function App() {
+import './App.scss'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Home from './Home/Home'
+import About from './About'
+import Contact from './Contact'
+import Commits from './Commits/Commits'
+
+export default function App() {
   return (
-    <div>
-      <h1>Start</h1>
-    </div>
+      <Router>             
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/commits">
+            <Commits />
+          </Route>
+        </Switch>      
+      </Router>
   );
 }
-
-export default App;

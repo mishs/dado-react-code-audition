26aba6b16afaac095a2f6b78a22d5ae5945d6e9d
Mish <mishuct26@gmail.com>
2021-04-11 21:13:56 +0200

    Updated ul styling

--- src/components/HeaderStyles.js
+++ src/components/HeaderStyles.js
@@ -37,8 +37,6 @@ export const HeaderStyles = styled.div`
         padding: 0;
         display: flex;
         align-items: center;
-        list-style: none;
-        padding: 0;
 
         li {
             margin-left: 64px;
